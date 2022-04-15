42998bb5e59bdbea12f1e5898ae3213ce83608ad
NaashNix <thilakshanaravindu@gmail.com>
2022-04-15 14:12:08 +0530

    Road Boudries Done

--- assets/stylingScript.js
+++ assets/stylingScript.js
@@ -1,51 +1,158 @@
-$("#gameSection").css("display", "none");
+let gameArea = document.querySelector(".gameArea");
+let car = document.querySelector("#car");
 
-let forward = 0;
-let actualHeight = window.innerHeight
-let actualWidth = window.innerWidth;
-// console.log(actualHeight);
 
-$("#homeSection").css("height", actualHeight);
+let height = window.innerHeight;
+let width = window.innerWidth;
 
-function gameStart() {
-    $("#homeSection").css("display", "none");
-    $("#gameSection").css("display", "block");
-    $("#roadContainer").css("height", actualHeight);
-}
+$("#backgroundImage").css("height", `${height}px `);
+$("#backgroundImage").css("width", `${width}px `);
+
+
+var roadRunningFunc = setInterval(() => {
+    gamePlay();
+}, 50);
+
 
 document.onkeydown = checkKey;
-
+start();
 function checkKey(e) {
 
-    let value = $("#gameSection").css("display");
+    // let value = $("#gameSection").css("display");
 
-    if (value != 'block') {
-        return;
-    }
+    // if (value != 'block') {
+    //     return;
+    // }
 
     e = e || window.event;
 
     if (e.keyCode == '38') {
-        console.log("");
+        carMoveForward();
     }
     else if (e.keyCode == '40') {
-        // down arrow
+        carMoveBackward();
     }
     else if (e.keyCode == '37') {
-        //Left Key
-        driveRoadForward();
+        turnCarRight();
     }
     else if (e.keyCode == '39') {
-        // right arrow
+        turnCarLeft();
     }
 
 }
 
+let j = 0;
 
-function driveRoadForward() {
+function gamePlay() {
+    let road = gameArea.getBoundingClientRect();
+    // console.log(road);
+
+    moveLines();
 
 }
 
-function roadRunning() {
+function moveLines() {
+    let lines = document.querySelectorAll(".lines");
+    lines.forEach(function (item) {
+        if (item.x >= 1200) {
+            item.x = 5;
+        }
+
+        item.x += 20;
+        item.style.right = item.x + 'px';
+    })
+}
+
+function start() {
+
+    window.requestAnimationFrame(gamePlay);
+
+    for (m = 0; m < 9; m++) {
+        let roadLines = document.createElement("div");
+        roadLines.setAttribute("class", "lines");
+        roadLines.x = (m * 150);
+        console.log(roadLines.x);
+        roadLines.style.right = roadLines.x + 'px';
+        gameArea.appendChild(roadLines);
+    }
+
+}
+
+function makeObstacles() {
+
+}
+
+function turnCarLeft() {
     
+    let road = gameArea.getBoundingClientRect();
+
+    var fromBottom = parseInt($("#car").css("top"));
+    let roadFromTop = parseInt($(".gameArea").css("top"));
+
+
+    if (fromBottom < gameArea.clientHeight + 40) {
+        fromBottom = fromBottom + 10;
+        $("#car").css("top", `${fromBottom}px`);
+    } else {
+        console.log("ELSE");
+    }
+
+
+
+
+}
+
+function turnCarRight() {
+    let road = gameArea.getBoundingClientRect();
+
+    let roadFromTop = parseInt($(".gameArea").css("top"));
+    var fromBottom = parseInt($("#car").css("top"));
+
+    console.log("From Bottom: ", fromBottom);
+
+    if (fromBottom > roadFromTop + 130) {
+        fromBottom = fromBottom - 10;
+        $("#car").css("top", `${fromBottom}px`);
+    } else {
+
+    }
+
+
+
+}
+
+function carMoveForward() {
+    let road = gameArea.getBoundingClientRect();
+
+
+    var fromLeft = parseInt($("#car").css("left"));
+    console.log("From left: ", fromLeft);
+    
+    console.log("Total Width : " ,road.left + 20 + gameArea.clientWidth);
+
+    if (gameArea.clientWidth - 95 > car.x) {
+        fromLeft = fromLeft + 10;
+        $("#car").css("left", `${fromLeft}px`);
+    } else {
+        console.log("Not Turned");
+    }
+    
+    
+    $("#car").css("left", `${fromLeft}px`);
+}
+
+function carMoveBackward() {
+    
+    let road = gameArea.getBoundingClientRect();
+
+    var fromLeft = parseInt($("#car").css("left"));
+    console.log("From left: ", fromLeft);
+
+    if (road.left + 20 < car.x) {
+        fromLeft = fromLeft - 10;
+        $("#car").css("left", `${fromLeft}px`);
+    } else {
+        console.log("Not Turned");
+    }
+
 }
